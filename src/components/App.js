76a6e18bf039cf35d55bb3af3883115210c96629
gunturuanugna <anugnagunturu1@gmail.com>
2021-08-18 20:37:55 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,24 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
+	constructor(props) {
 		super(props);
+		this.state = {
+			pHidden : true,
+		}
 	};
+
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				<button id="click" onClick={ ()=>{
+					this.setState({pHidden : false})
+				}}>Show Para</button>
+				{this.state.pHidden ? null : (
+					<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+				)}
+				
     		</div>
     	);
     }
